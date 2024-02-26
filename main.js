504bf55296e7446ba6c4009a337b1e635aeb89e0
Anna Kostrytsa <annakostrytsa@Annas-MacBook-Pro.local>
2024-02-26 15:38:43 +0100

    ading details about db and html

---
+++ main.js
@@ -0,0 +1,4 @@
+function redirectToSliderPage() {
+  // Redirect to the slider page
+  window.location.href = "slider_page.html"; // Change "slider_page.html" to the actual URL of your slider page
+}
